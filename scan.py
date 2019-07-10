7a49ac1878cc6a71087988bfad4fce249579037c
Connor Howington <connor.howington@gmail.com>
2019-07-09 20:11:30 -0400

    Work in progress: Pull from database

--- scan.py
+++ scan.py
@@ -6,6 +6,7 @@ import argparse
 import csv
 import os
 import random
+import requests
 
 
 def parse_args():
@@ -13,7 +14,6 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Transforms a VectorBase search export file to '
                                                  'SCAN DwC format.')
 
-    parser.add_argument('input', help='The CSV input file.')
     parser.add_argument('output', help='The name of the output file.')
     parser.add_argument('-s', '--sample', type=float,
                         help='Output a sample of the input data. Provide a number from 0-100 '
@@ -27,15 +27,36 @@ def parse_args():
     return args
 
 
-def make_scan(input, output, sample=None):
+def make_scan(output, sample=None):
     if sample:
         random.seed()
 
     output_rows = [
         'occurrenceID', 'catalogNumber', 'dataGeneralizations', 'basisOfRecord', 'individualCount',
-        'sex', 'lifeStage', 'references', 'eventDate', 'verbatimEventDate', 'samplingProtocol',
-        'country', 'stateProvince', 'locality', 'decimalLatitude', 'decimalLongitude',
-        'identificationRemarks', 'scientificName', 'identificationQualifier', 'occurrenceRemarks'
+        'sex', 'lifeStage', 'references', 'recordedBy', 'eventDate', 'verbatimEventDate',
+        'samplingProtocol', 'country', 'stateProvince', 'locality', 'decimalLatitude',
+        'decimalLongitude', 'identificationRemarks', 'scientificName', 'identificationQualifier',
+        'occurrenceRemarks'
+    ]
+
+    solr_fields = [
+        'sample_id_s',
+        'projects',
+        'species',
+        'geo_coords',
+        'country_s',
+        'adm1_s',
+        'adm2_s',
+        'collection_protocols',
+        'collection_day_s',
+        'collection_date_range',
+        'protocols',
+        'sample_size_i',
+        'exp_citations_ss',
+        'tags_ss',
+        'sex_s',
+        'dev_stages_ss',
+        'project_authors_txt'
     ]
 
     valid_first_species_terms = (
@@ -50,8 +71,6 @@ def make_scan(input, output, sample=None):
 
     group_terms = ('morphological', 'group', 'complex', 'sensu', 'lato', 'AD', 'BCE', 'subgroup')
 
-    countries = ('United States', 'United Kingdom', 'Uganda')
-
     valid_provider_tags = (
         'Anastasia Mosquito Control District Florida',
         'Marion County Public Health Department Indiana',
@@ -63,7 +82,8 @@ def make_scan(input, output, sample=None):
         'Orange County Florida Mosquito Control', 'ICEMR',
         'Rhode Island Department of Environmental Management',
         'South Walton County Florida Mosquito Control', 'Toledo Area Sanitary District',
-        'Ada County Weed Pest and Mosquito Abatement'
+        'Ada County Weed Pest and Mosquito Abatement',
+        'Marion County Public Health Department, Indiana'
     )
 
     remove_tags = ('abundance', 'viral surveillance')
@@ -71,46 +91,85 @@ def make_scan(input, output, sample=None):
     temp = output + '.temp'
 
     try:
-        with open(input) as input_file, open(temp, 'w', newline='') as temp_file:
-            input_csv = csv.DictReader(input_file)
+        rows = 5000
+
+        url = 'http://localhost:8200/solr/collection1/select'
+        url += '?fl=' + ','.join(solr_fields)
+        url += ('&fq=bundle:pop_sample' +
+                '&fq=has_abundance_data_b:true' +
+                '&fq=has_geodata:true' +
+                '&fq=sample_size_i:%5B1%20TO%20*%5D' +
+                '&fq=site:%22Population%20Biology%22'
+                )
+        url += '&q=*:*'
+        url += '&rows=' + str(rows)
+
+        print('Sending request...')
+        response = requests.get(url)
+        print('Done.')
+        response.raise_for_status()
+        js = response.json()
+
+        with open(temp, 'w', newline='') as temp_file:
             temp_csv = csv.DictWriter(temp_file, output_rows)
 
             temp_csv.writeheader()
 
-            for row in input_csv:
+            i = 0
+
+            for record in js['response']['docs']:
+                defaults = {
+                    'projects': [''],
+                    'country_s': '',
+                    'adm1_s': '',
+                    'adm2_s': '',
+                    'collection_protocols': [''],
+                    'collection_date_range': [''],
+                    'protocols': [''],
+                    'exp_citations_ss': [''],
+                    'sex_s': '',
+                    'dev_stages_ss': [''],
+                    'project_authors_txt': ['']
+                }
+
+                record.update(defaults)
+
+                print(i)
+                i += 1
+
                 # If we're sampling, drop the row with some probability
                 if sample is not None and random.random() >= sample/100:
                     continue
 
                 # Discard the row based on certain conditions
-                if (not row['has_geodata']
-                        or row['collection_protocols'] == 'BG-Counter trap catch'):
+                if record['collection_protocols'] == 'BG-Counter trap catch':
                     continue
 
-                # Directly set the fields that need no processing
+                # Directly set the fields that need little processing
                 output_row = {
-                    'occurrenceID': row['accession'],
-                    'catalogNumber': row['accession'],
-                    'dataGeneralizations': row['projects'],
+                    'occurrenceID': record['sample_id_s'],
+                    'catalogNumber': record['sample_id_s'],
+                    'dataGeneralizations': ';'.join(record['projects']),
                     'basisOfRecord': 'HumanObservation',
-                    'individualCount': row['sample_size_i'],
-                    'sex': row['sex_s'],
-                    'lifeStage': row['dev_stages_ss'],
-                    'references': row['exp_citations_ss'],
-                    'verbatimEventDate': row['collection_date_range'],
-                    'samplingProtocol': row['collection_protocols'],
-                    'identificationRemarks': row['protocols'],
+                    'individualCount': record['sample_size_i'],
+                    'sex': record['sex_s'],
+                    'lifeStage': ';'.join(record['dev_stages_ss']),
+                    'references': record['exp_citations_ss'],
+                    'recordedBy': ';'.join(record['project_authors_txt']),
+                    'verbatimEventDate': ';'.join(record['collection_date_range']),
+                    'samplingProtocol': ';'.join(record['collection_protocols']),
+                    'identificationRemarks': ';'.join(record['protocols']),
                 }
 
                 # Species
-                species_terms = row['species'].split()
+                species_terms = record['species'][0].split()
 
                 if species_terms[0] == 'genus' or species_terms[0] == 'subgenus':
                     del species_terms[0]
 
                 if species_terms[0] not in valid_first_species_terms:
                     raise ValueError('Unknown first species term "{}" at {}'
-                                     .format(species_terms[0], row['accession']))
+                                     .format(species_terms[0], record['sample_id_s']))
 
                 output_row['scientificName'] = species_terms[0]
 
@@ -124,7 +183,7 @@ def make_scan(input, output, sample=None):
                             output_row['identificationQualifier'] = species_terms[2]
                         else:
                             ValueError('Unknown third species term "{}" at {}'
-                                       .format(species_terms[2], row['accession']))
+                                       .format(species_terms[2], record['sample_id_s']))
 
                         if len(species_terms) == 4:
                             if 'identificationQualifier' not in output_row:
@@ -133,52 +192,39 @@ def make_scan(input, output, sample=None):
                             output_row['identificationQualifier'] += ' ' + species_terms[3]
 
                 # Coordinates
-                coordinates = row['geo_coords'].split(',')
+                coordinates = record['geo_coords'].split(',')
                 output_row['decimalLatitude'] = coordinates[0]
                 output_row['decimalLongitude'] = coordinates[1]
 
                 # Location
-                location_terms = row['geolocations'].strip(')').split(' (')
-
-                output_row['locality'] = location_terms[0]
-
-                if len(location_terms) >= 2:
-                    if location_terms[1] in countries:
-                        output_row['country'] = location_terms[1]
-                    else:
-                        output_row['stateProvince'] = location_terms[1]
-
-                        if len(location_terms) == 3:
-                            if location_terms[2] in countries:
-                                output_row['country'] = location_terms[2]
-                            else:
-                                raise ValueError('Unknown third location term "{}" at {}'
-                                                 .format(location_terms[2], row['accession']))
+                output_row['country'] = record['country_s'].split(' (')[0]
+                output_row['stateProvince'] = record['adm1_s'].split(' (')[0]
+                output_row['locality'] = record['adm2_s'].split(' (')[0]
 
                 # Date
-                output_row['eventDate'] = row['collection_date'][:10]
+                output_row['eventDate'] = record['collection_day_s'][:10]
 
                 # occurrenceRemarks
                 author_text = generator_text = ''
                 link = 'https://www.vectorbase.org/popbio/map/?view=abnd&zoom_level=11'
-                link += '&center=' + row['geo_coords']
+                link += '&center=' + record['geo_coords']
 
-                if row['tags_ss']:
-                    tags = row['tags_ss'].split(';')
-                    tags = [tag for tag in tags if tag not in remove_tags]
+                if 'tags_ss' in record:
+                    tags = [tag for tag in record['tags_ss'] if tag not in remove_tags]
 
                     if len(tags) > 1 or tags[0] not in valid_provider_tags:
                         raise ValueError('Unexpected tag(s) {} at {}'
-                                         .format(tags, row['accession']))
+                                         .format(tags, record['sample_id_s']))
 
                     author_text += ' authored by ' + tags[0]
                     link += '&tag=' + tags[0].replace(' ', '+')
                 else:
-                    link += '&projectID=' + row['projects']
+                    for project in record['projects']:
+                        link += '&projectID[]=' + project
 
-                if row['exp_citations_ss']:
+                if record['exp_citations_ss']:
                     generator_text += (', which includes '
-                                       + '; '.join(row['exp_citations_ss'].split(';')))
+                                       + '; '.join(record['exp_citations_ss']))
 
                 output_row['occurrenceRemarks'] = (
                     'This record has been curated by VectorBase.org as part of a larger data set{}'
